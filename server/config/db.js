a0143b4f0cabbbac66845436cd068d4396d743be
gobinddhaliwal <dhaliwalgobind@gmail.com>
2019-04-06 15:29:54 -0400

    mlab

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-   "URI" : "mongodb://localhost/app"
-   // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
+  // "URI" : "mongodb://localhost/app"
+    "URI" : "mongodb://gobind:gobind1@ds115870.mlab.com:15870/inventrac"
 }
